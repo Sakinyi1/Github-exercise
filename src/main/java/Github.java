6c069d41f67126bebdc65e87781e2ee0d7ad2fb1
Sandra Akinyi <sakinyi100@gmail.com>
2019-07-08 09:43:32 -0500

    first commit

---
+++ src/main/java/Github.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+
+public class Github {
+    public static void main(String[] args){
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        System.out.println(num1 + num2);
+
+    }
+}
